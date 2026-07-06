b41a326c509d08068babbedbbc9c03d608341310
Felix Harding <j.felixharding@gmail.com>
2026-07-06 17:01:02 +1000

    Fix comment formatting in 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) /*simple print function, puts automatically adds a newline at the end*/
+int main(void)
+/*simple print function, puts automatically adds a newline at the end*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
