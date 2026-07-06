f90d49516520670f6c456638478c5a0d86754422
Felix Harding <j.felixharding@gmail.com>
2026-07-06 16:58:50 +1000

    Refactor main function comment placement

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*simple print function, puts automatically adds a newline at the end*/
-int main(void)
+
+int main(void) /*simple print function, puts automatically adds a newline at the end*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
