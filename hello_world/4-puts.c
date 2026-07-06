31df40343bec814586f96b1043c2842613d2e393
Felix Harding <j.felixharding@gmail.com>
2026-07-06 17:05:10 +1000

    Change comments to Doxygen format in 4-puts.c
    Updated comment style from block to Doxygen format.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
-*main is simple print function
-*puts automatically adds a newline at the end
-*return sets the value back to 0
-*/
+/**
+ *main is simple print function
+ *puts automatically adds a newline at the end
+ *return sets the value back to 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
