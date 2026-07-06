8b20ed0bc5cafff4157af4c9b7e88fc32e4e10cc
Felix Harding <j.felixharding@gmail.com>
2026-07-06 17:03:44 +1000

    Enhance comments in 4-puts.c
    Added comments to explain the main function and puts usage.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+*main is simple print function
+*puts automatically adds a newline at the end
+*return sets the value back to 0
+*/
 int main(void)
-/*simple print function, puts automatically adds a newline at the end*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
