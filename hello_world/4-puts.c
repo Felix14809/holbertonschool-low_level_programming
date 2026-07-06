163ec75b7e93400db8f212b039af3485fe12d527
Felix Harding <j.felixharding@gmail.com>
2026-07-06 17:09:04 +1000

    Fix comments in 4-puts.c for clarity
    Corrected comments for clarity and formatting.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *main is simple print function
- *puts automatically adds a newline at the end
- *return sets the value back to 0
+ * main - a simple print function
+ * puts automatically adds a newline at the end
+ *
+ * Return: sets the value back to 0
  */
 int main(void)
 {
