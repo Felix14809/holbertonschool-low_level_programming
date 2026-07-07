86efc50561f641bbde887e19a6a71f23f949b61c
Felix Harding <j.felixharding@gmail.com>
2026-07-07 12:57:08 +1000

    test

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - declares a character
+ * then runs a while loop
+ * until z and not q and e is printed
+ *
+ * Return: is set back to 0
+ */
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+		if(lower !='e','q')
+		{
+			putchar(lower):
+		}
+
+		lower++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
