5fc2b7e8f1199d6be50ad96b8fd9885aa39f8583
Felix Harding <j.felixharding@gmail.com>
2026-07-07 12:29:47 +1000

    fixes

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
- * main - declares a character
- * then runs a do while loop
- * until character "z" is printed
+ * main - declares an integer
+ * then runs a while loop
+ * until z is printed
  *
  * Return: is set back to 0
  */
 int main(void)
 {
-    int i = 97;
-
-    while (i <= 122)
-    {
-    putchar (i);
-    i++;
-    }
-    putchar ('\n');
-    return (0);
+	int i = 97;
+	while (i <= 122)
+	{
+	putchar (i);
+	i++;
+	}
+	putchar ('\n');
+	return (0);
 }
