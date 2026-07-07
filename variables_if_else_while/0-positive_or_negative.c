60b0d088b45ea18d66148d86fd9eed4145e9d4e5
Felix Harding <j.felixharding@gmail.com>
2026-07-07 11:01:49 +1000

    first if else program

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#inlcude<stdio.h>
+/**
+ * main - includes an if else function
+ * prints different messages and the variable
+ * based on the number assigned to "n"
+ *
+ * Return is set back to 0
+ */
+
+main()
+{
+	if (n>0)
+	{
+		printf("%d\n is positive\n", n);
+	}
+	else if (n<0)
+	{
+		printf("%d\n is negative\n", n);
+	}
+	else
+	{
+		printf("%d\n is zero\n"), n);
+	}
+    return (0);
+
+}
