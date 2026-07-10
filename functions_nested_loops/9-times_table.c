a6bf054e5d33274d8697832b8106c99df5d0cd80
Felix Harding <j.felixharding@gmail.com>
2026-07-10 15:58:23 +1000

    Fix condition for printing leading zero in times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,11 +22,13 @@ void times_table(void)
 				_putchar(total / 10 + '0');
 				_putchar(total % 10 + '0');
 			}
-			else if ((total < 10 && dig1 != 0) || (dig1 != 0 && dig2 >= 0))
+			else if ((total < 10 && dig1 != 0) || (total < 10 && dig2 > 0))
 			{
 				_putchar(' ');
 				_putchar(total + '0');
 			}
+			else
+				_putchar('0');
 			if (dig2 < 9)
 			{
 				_putchar(',');
