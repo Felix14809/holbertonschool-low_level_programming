58f5ab54a840add819841c169f67629e030ec03d
Felix Harding <j.felixharding@gmail.com>
2026-07-12 16:04:43 +1000

    Modify more_numbers to print 0-14 ten times
    Refactor more_numbers function to print digits from 0 to 14 ten times.

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,26 @@
 #include "main.h"
 /**
- * more_numbers - prints digits from 0 to 14
+ * more_numbers - prints digits from 0 to 14 10 times
  *
  * Return: void
  */
 void more_numbers(void)
 {
 	int number = 0;
-
-	while (number < 15)
-	{
-		if (number > 9)
+	int times = 0;
+	
+	while (times < 10)
+	{	
+		while (number < 15)
 		{
-			_putchar(number / 10 + '0');
+			if (number > 9)
+			{
+				_putchar(number / 10 + '0');
+			}
+			_putchar(number % 10 + '0');
+			number++;
 		}
-		_putchar(number % 10 + '0');
-		number++;
+		times++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
