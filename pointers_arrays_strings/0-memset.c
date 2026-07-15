94c6fdba5384f53d79a211c7f0ad0c5b0b371cb1
Felix Harding <j.felixharding@gmail.com>
2026-07-15 15:23:49 +1000

    test

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_memset - fills n bytes of *s with b
+ * @*s: the memory area pointed to
+ * @b: the byte to add
+ * @n: the number of bytes to fill
+ *
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *start;
+
+	start = s;
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (start);
+}
