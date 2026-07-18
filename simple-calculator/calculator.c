480056a935df09dd5a5d8aeeb7a9a8e7c4456270
Felix Harding <j.felixharding@gmail.com>
2026-07-18 18:10:11 +1000

    task 1

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A simple calculator that runs in your terminal
+ * 
+ * 
+ *
+ * Return: void
+ */
+int main(void)
+{
+	printf("Choose how you want to calculate your numbers \n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit\n");
+	return (0);
+}
