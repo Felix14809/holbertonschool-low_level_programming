48d3f47297fd66e409a6f41397c95da0749c5392
Felix Harding <j.felixharding@gmail.com>
2026-07-19 00:17:08 +1000

    finished calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- * main - A simple calculator that runs in your terminal
- * 
- * 
+ * main - A simple terminal calculator 
  *
  * Return: void
  */
@@ -10,26 +8,26 @@ int main(void)
 {
 	float a, b, sum;
 	int option;
-	
+
 	printf("Choose how you want to calculate your numbers \n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n5) Exit\n");
 	while (scanf("%d", &option) != 1 || option < 1 || option > 5)
 	{
 		while (getchar() != '\n')
-    		;
+		;
 		printf("Invalid input. Try again or enter 5 to exit\n");
-	}	
+	}
 	if (option == 5)
 	{
 		printf("Bye for now!");
 		return (0);
 	}
-	else 
-		printf("You choice is: %d\nEnter two numbers separated by a space:", option);	
+	else
+		printf("You chose: %d\nEnter two numbers separated by a space:", option);
 	while (scanf("%f %f", &a, &b) != 2)
 	{
 		while (getchar() != '\n')
-    			;
-			printf("Invalid input. Try again or press Ctrl + C to terminate\n");
+			;
+		printf("Invalid input. Try again or press Ctrl + C to terminate\n");
 	}
 	if (option == 1)
 		sum = a + b;
@@ -38,13 +36,13 @@ int main(void)
 	else if (option == 3)
 		sum = a * b;
 	else if (option == 4)
-	{	
+	{
 		if (b == 0)
 		{
 			printf("ERROR: division by 0\n");
 			return (0);
 		}
-		sum = a / b;	
+		sum = a / b;
 	}
 	if (sum == (int)sum)
 		printf("Result: %d\n", (int)sum);
