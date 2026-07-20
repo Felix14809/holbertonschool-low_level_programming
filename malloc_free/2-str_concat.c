21c5482fb0800cac8b7642c00f007ab1690855fd
Felix Harding <j.felixharding@gmail.com>
2026-07-20 18:57:56 +1000

    Fix syntax error in string concatenation function

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	if (s1 == NULL && s2 == NULL))
-		return (NULL)
+		return (NULL);
 	else if (s1 == NULL)
 		*s1 = '\0';
 	else if (s2 == NULL)
