0e21524bd0c7776891303b776e127564732d47d5
Felix Harding <j.felixharding@gmail.com>
2026-07-20 19:23:01 +1000

    Simplify null checks for string concatenation

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,11 +15,9 @@ char *str_concat(char *s1, char *s2)
 	s = 0;
 	i = 0;
 	j = 0;
-	if ((s1 == NULL) && (s2 == NULL))
-		return (NULL);
-	else if (s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 	while (s1[i])
 		i++;
