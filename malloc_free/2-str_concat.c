6a72d57985c11a239dfb94eb68a105b5d15eb4b0
Felix Harding <j.felixharding@gmail.com>
2026-07-20 19:01:39 +1000

    Update NULL string handling in str_concat function
    Fix handling of NULL strings by assigning empty strings instead of null terminators.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	else if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "";
 	else if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
