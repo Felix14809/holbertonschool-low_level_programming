51acdbe4b9bba91bd1462cde90dfeb53c2f457c5
Felix Harding <j.felixharding@gmail.com>
2026-07-20 18:50:58 +1000

    Update 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,13 +15,13 @@ char *str_concat(char *s1, char *s2)
 	s = 0;
 	i = 0;
 	j = 0;
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
 	while (s1[i])
 		i++;
 	while (s2[j])
 		j++;
 	concstr = malloc(i + j + 1);
+	if (concstr == NULL)
+		return (NULL);
 	while (*s1)
 	{
 		concstr[s] = *s1;
