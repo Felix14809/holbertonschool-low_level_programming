38d3619db9b107f088957751d3284f67a14f9d6f
Felix Harding <j.felixharding@gmail.com>
2026-07-22 16:21:40 +1000

    (nil) not nil

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,19 +2,19 @@
 #include "dog.h"
 /**
  * print_dog - prints a structure
- * @dog *d: the structure passed
+ * @d: the structure passed
  *
  * Return: nothin
  */
 void print_dog(struct dog *d)
 {
 	if (!(d->name))
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (!(d->owner))
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
