1efd44888bd9eb89d78f54f0b062c4e39ac17937
Felix Harding <j.felixharding@gmail.com>
2026-07-29 17:50:05 +1000

    bety plz

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator != NULL && i != n -1)
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 		i++;
 	}
