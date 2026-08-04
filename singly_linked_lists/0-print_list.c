e6a0a028e6f5049b0001052363434ea9b20d8a1a
Felix Harding <j.felixharding@gmail.com>
2026-08-04 13:29:46 +1000

    task 1, readme, header file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * print_list - prints all elements in the list
+  *@h: structure
+  *Return: number of nodes 
+  */
+size_t print_list(const list_t *h)
+{
+	unsigned long int elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+    if (h->str == NULL)
+      printf("[%d] %s\n", 0, "(nil)");
+    else
+		  printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
