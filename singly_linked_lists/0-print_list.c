eca3e084b213ce2e62f6e9776126b6e5d7cb8425
Felix Harding <j.felixharding@gmail.com>
2026-08-04 13:40:40 +1000

    bettyplz

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
-  * print_list - prints all elements in the list
-  *@h: structure
-  *Return: number of nodes 
-  */
+* print_list - prints all elements in the list
+*@h: structure
+*Return: number of nodes
+*/
 size_t print_list(const list_t *h)
 {
 	unsigned long int elements;
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 	elements = 0;
 	while (h != NULL)
 	{
-    if (h->str == NULL)
-      printf("[%d] %s\n", 0, "(nil)");
-    else
-		  printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		elements++;
+	if (h->str == NULL)
+		printf("[%d] %s\n", 0, "(nil)");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	elements++;
 	}
 	return (elements);
 }
