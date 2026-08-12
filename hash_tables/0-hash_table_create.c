8b39458e6f0166e900a2de75403d2f6760399fe0
Felix Harding <j.felixharding@gmail.com>
2026-08-12 15:18:02 +1000

    first task, header, readme

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: of the table
+ * Return: pointer to new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long int i = 0;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+		return (NULL);
+	i++;
+	return (new);
+}
