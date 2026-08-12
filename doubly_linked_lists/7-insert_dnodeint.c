b24234e72d688aea8d17b48efd91d1d0e3962315
Felix Harding <j.felixharding@gmail.com>
2026-08-12 14:07:11 +1000

    all tasks

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		new->prev = temp;
-		new->next = temp->next;	
+		new->next = temp->next;
 		if (temp->next != NULL)
 			temp->next->prev = new;
 		temp->next = new;
