20b26b293c64bd0609a250a2a835f2baf272abf3
Felix Harding <j.felixharding@gmail.com>
2026-08-17 13:25:20 +1000

    recursion start

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts_recursion - printe a string using recursion
+*@s: the string
+*/
+void _puts_recursion(char *s)
+{
+	if (!*s)
+		_putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
