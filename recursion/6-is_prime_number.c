4c637bb67d4777d387f758c851382d7211fabec9
Felix Harding <j.felixharding@gmail.com>
2026-08-21 11:27:21 +1000

    some more tasks

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_prime_number - calculates if the number is prime
+ *@n: number to check
+ *Return: 1 if yes otherwise 0
+ */
+int is_prime_number(int n)
+{
+	int i = 1;
+	float prime
+	if ((is_prime_number(n) / i) == (int))
+}
