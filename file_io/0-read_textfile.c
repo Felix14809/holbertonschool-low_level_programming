69f541e8e9e653ca22f53477ae62127ebe50926e
Felix Harding <j.felixharding@gmail.com>
2026-08-21 17:44:42 +1000

    first task

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*read_textfile - read and prints a file to standard output
+*@filename: name of the file
+*@letters: number of characters to read
+*Return: 0 or the number of letters that could be read and printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t ltrsread;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	ltrsread = read(fd, buffer, letters);
+	if (ltrsread == -1)
+		return (0);
+	write(STDOUT_FILENO, buffer, ltrsread);
+	free(buffer);
+	return (ltrsread);
+}
