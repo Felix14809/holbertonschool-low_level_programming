181fd8f37ed341c7ace1f096ba293982ea45d9f0
Felix Harding <j.felixharding@gmail.com>
2026-08-22 14:41:07 +1000

    forgotten task

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
-* hash_table_get - get the value element associated with a key from the hash table
+* hash_table_get - get the value element associated with a key from ht
 * @ht: pointer to the hashtable
 * @key: the key
 * Return: pointer to value
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (key != NULL && ht != NULL)
 		hash = hash_djb2((const unsigned char *)key) % ht->size;
-	else 
+	else
 		return (NULL);
 	if (ht->array[hash] == NULL)
 		return (NULL);
