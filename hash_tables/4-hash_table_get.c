9101e729412c74327b0a7a431ee0637e963680a3
Felix Harding <j.felixharding@gmail.com>
2026-08-22 14:39:44 +1000

    forgotten task

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,16 +10,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int hash;
 	hash_node_t *node;
 
-	if (key != NULL)
+	if (key != NULL && ht != NULL)
 		hash = hash_djb2((const unsigned char *)key) % ht->size;
+	else 
+		return (NULL);
 	if (ht->array[hash] == NULL)
 		return (NULL);
 	node = ht->array[hash];
-	while (node->key != key)
+	while (strcmp(node->key, key) != 0 && node->next != NULL)
 	{
 		node = node->next;
-		if (node->key = key)
-			return (node->key);
 	}
+	if (strcmp(node->key, key) == 0)
+		return (node->value);
 	return (NULL);
 }
